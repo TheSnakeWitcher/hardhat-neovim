942b568433d5804f9da030768ec9e818663260fb
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-17 11:07:11 -0500

    fix: use types in task definition

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const TASK_TEST = "test";
 const NEOTEST_HARDHAT_REPORTER_PATH = "./node_modules/hardhat-neovim/neotest-hardhat-reporter.js"
 
 task(TASK_TEST)
-  .addFlag("neovim","flag to run test using neotest-hardhat-reporter and produce json output easily consumable by the neotest-hardhat-adapter")
-  .setAction(async function(args, hre, runSuper) {
+  .addFlag("neovim","flag to run test using neotest-hardhat-reporter and produce json output easily consumable by the neotest-hardhat adapter")
+  .setAction(async function(args: any, hre: HardhatRuntimeEnvironment, runSuper: any) {
     if (args.neovim) {
       hre.config.mocha.reporter = NEOTEST_HARDHAT_REPORTER_PATH
       await runSuper(args);
