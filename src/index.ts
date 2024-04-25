5e707ebace7929a7698567704816dea44d449507
Mr.PAPI <thesnakewitcher@gmail.com>
2024-04-24 20:55:11 -0400

    style: indent src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@ const TASK_TEST = "test";
 const NEOTEST_HARDHAT_REPORTER_PATH = "./node_modules/hardhat-neovim/neotest-hardhat-reporter.js"
 
 task(TASK_TEST)
-  .addFlag("neovim","flag to run test using neotest-hardhat-reporter and produce json output easily consumable by the neotest-hardhat adapter")
-  .setAction(async function(args: any, hre: HardhatRuntimeEnvironment, runSuper: any) {
-    if (args.neovim) {
-      hre.config.mocha.reporter = NEOTEST_HARDHAT_REPORTER_PATH
-      await runSuper(args);
-    } else {
-        await runSuper(args);
-    }
-})
+    .addFlag("neovim", "flag to run test using neotest-hardhat-reporter and produce json output easily consumable by the neotest-hardhat adapter")
+    .setAction(async function(args: any, hre: HardhatRuntimeEnvironment, runSuper: any) {
+        if (args.neovim) {
+            hre.config.mocha.reporter = NEOTEST_HARDHAT_REPORTER_PATH
+            await runSuper(args);
+        } else {
+            await runSuper(args);
+        }
+    })
